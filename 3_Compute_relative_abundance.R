39c05c07ae1959fe00af72b8c4bb9d539e76a798
Grant Vernham <g.vernham@outlook.com>
2024-09-29 21:06:33 +0100

    Script to compute relative abundance from raw pollen counts

---
+++ 3_Compute_relative_abundance.R
@@ -0,0 +1,34 @@
+# Load libraries
+libs <- c("dplyr")
+
+installed_libs <- libs %in% rownames(
+  installed.packages())
+
+if (any(installed_libs == F)) {
+  install.packages(
+    libs[!installed_libs]
+  )
+}
+
+invisible(lapply(
+  libs,
+  library,
+  character.only = T
+))
+rm(list=ls())
+
+# Load functions
+source("./Functions/Compute_relative_abundance.R")
+
+# Load raw data
+load("./Raw_Data/bigdf_familynames.Rda")
+time_ID = bigdf_familynames[c("dataset_ID", "meantimes")] # subset time/site data
+
+# Apply data quality control and compute relative abundance
+pollen_relative_abun <- bigdf_familynames %>%
+  {bigdf_familynames[!duplicated(colnames(bigdf_familynames))]} %>% # "Cyperaceae" is duplicated in the data.
+  dplyr::select(-colnames(time_ID)) %>%
+  as.matrix() %>%
+  compute_relative_abundance() %>%
+  cbind(time_ID) %>%
+  dplyr::select(colnames(time_ID), everything())
