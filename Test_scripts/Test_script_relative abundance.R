5083643bbc62e3f8eded5104fd22f33e1d6d2632
Grant Vernham <g.vernham@outlook.com>
2024-09-29 21:02:58 +0100

    Initial commit

--- Test_scripts/Test_script_relative abundance.R
+++ Test_scripts/Test_script_relative abundance.R
@@ -22,16 +22,16 @@ source("./Functions/Compute_relative_abundance.R")
 
 # Load raw data
 load("./Raw_Data/bigdf_familynames.Rda")
+time_ID = bigdf_familynames[c("dataset_ID", "meantimes")] # subset time/site data
 
 # Data quality control and cleaning
-bigdf_familynames = bigdf_familynames[!duplicated(colnames(bigdf_familynames))] # Species "Cyperaceae" is duplicated in the data.
-time_ID = bigdf_familynames[c("dataset_ID", "meantimes")] # Subset data to be removed from relative abundance calculation
-raw_pollen = bigdf_familynames %>% select(-colnames(time_ID))
-raw_pollen = as.matrix(raw_pollen)
-
-# Calculate relative abundance
-pollen_relative_abun = compute_relative_abundance(raw_pollen)
-pollen_relative_abun = cbind(time_ID, data.frame(pollen_relative_abun)) # re-bind time and site data
+pollen_relative_abun <- bigdf_familynames %>%
+  {bigdf_familynames[!duplicated(colnames(bigdf_familynames))]} %>% # "Cyperaceae" is duplicated in the data.
+  dplyr::select(-colnames(time_ID)) %>%
+  as.matrix() %>%
+  compute_relative_abundance() %>%
+  cbind(time_ID) %>%
+  dplyr::select(colnames(time_ID), everything())
 
 
 
