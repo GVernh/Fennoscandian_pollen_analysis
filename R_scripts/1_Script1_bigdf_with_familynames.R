6e1c588f79a1d9a95e1801615e26a1f2e804d329
Grant Vernham <g.vernham@outlook.com>
2025-01-17 13:04:45 +0000

    Added code check for raw data files and unzip raw data if needed

--- R_scripts/1_Script1_bigdf_with_familynames.R
+++ R_scripts/1_Script1_bigdf_with_familynames.R
@@ -20,14 +20,17 @@ invisible(lapply(
 ))
 rm(list=ls())
 
-
+if (!(paste0("Temp12k_v1_0_0.RData") %in% list.files("./Raw_Data/"))){
 zipF<- "./Raw_Data/Temp12k_v1_0_0.RData.zip"
 outDir<-"./Raw_Data/"
 unzip(zipF,exdir=outDir)
+}
 
+if (!(paste0("p3k14c_original.csv") %in% list.files("./Raw_Data/"))) {
 zipF<- "./Raw_Data/p3k14c_original.csv.zip"
 outDir<-"./Raw_Data/"
 unzip(zipF,exdir=outDir)
+}
 
 # Source functions
 source("./Functions/MakestandardDF.R")
