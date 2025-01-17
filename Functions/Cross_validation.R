b6375df938e9fa8e6935e711d1500d9888dfb047
Grant Vernham <g.vernham@outlook.com>
2025-01-17 14:45:54 +0000

    Corrections

--- Functions/Cross_validation.R
+++ Functions/Cross_validation.R
@@ -11,7 +11,7 @@ crossvalidation = function(LCC, SPD, clim){
   chunksize = ceiling(nrow(VAR_data)/(nfolds+1))
   BP = -yearsBP[(maxlag + 1):length(yearsBP)] # so that time goes up with time
   VAR_data_ext = cbind(BP,VAR_data)
-  tscv <- time_series_cv(data = VAR_data_ext, date_var = BP, initial = chunksize, skip = chunksize - 1, assess = chunksize - 1, cumulative = TRUE)
+  tscv <- timetk::time_series_cv(data = VAR_data_ext, date_var = BP, initial = chunksize, skip = chunksize - 1, assess = chunksize - 1, cumulative = TRUE)
   
   for(i in 1:nfolds){
     dataconN_cv$train[[i]]$idx <- tscv$splits[[i]]$in_id
@@ -21,52 +21,52 @@ crossvalidation = function(LCC, SPD, clim){
   }
   # the full model with LCC, SPD and clim
   formula7 <- paste(colnames(VAR_data)[-1], collapse = " + ")
-  formula7 <- formula(paste0(varnames[1], " ~ ", formula7))
+  formula7 <- stats::formula(paste0(varnames[1], " ~ ", formula7))
   
   formula6 <- dplyr::select(VAR_data,-contains("7"))
   formula6 <- paste(colnames(formula6)[-1], collapse = " + ")
-  formula6 <- formula(paste0(varnames[1], " ~ ", formula6))
+  formula6 <- stats::formula(paste0(varnames[1], " ~ ", formula6))
   
   formula5 <- dplyr::select(VAR_data,-contains(c("7", "6")))
   formula5 <- paste(colnames(formula5)[-1], collapse = " + ")
-  formula5 <- formula(paste0(varnames[1], " ~ ", formula5))
+  formula5 <- stats::formula(paste0(varnames[1], " ~ ", formula5))
   
   formula4 <- dplyr::select(VAR_data,-contains(c("7", "6", "5")))
   formula4 <- paste(colnames(formula4)[-1], collapse = " + ")
-  formula4 <- formula(paste0(varnames[1], " ~ ", formula4))
+  formula4 <- stats::formula(paste0(varnames[1], " ~ ", formula4))
   
   formula3 <- dplyr::select(VAR_data,-contains(c("7", "6", "5", "4")))
   formula3 <- paste(colnames(formula3)[-1], collapse = " + ")
-  formula3 <- formula(paste0(varnames[1], " ~ ", formula3))
+  formula3 <- stats::formula(paste0(varnames[1], " ~ ", formula3))
   
   formula2 <- dplyr::select(VAR_data,-contains(c("7", "6", "5", "4", "3")))
   formula2 <- paste(colnames(formula2)[-1], collapse = " + ")
-  formula2 <- formula(paste0(varnames[1], " ~ ", formula2))
+  formula2 <- stats::formula(paste0(varnames[1], " ~ ", formula2))
   
   formula1 <- dplyr::select(VAR_data,-contains(c("7", "6", "5", "4", "3", "2")))
   formula1 <- paste(colnames(formula1)[-1], collapse = " + ")
-  formula1 <- formula(paste0(varnames[1], " ~ ", formula1))
+  formula1 <- stats::formula(paste0(varnames[1], " ~ ", formula1))
   
-  model7  <- map(dataconN_cv$train, ~glm2(formula7, data = .))
-  model6  <- map(dataconN_cv$train, ~glm2(formula6, data = .))
-  model5  <- map(dataconN_cv$train, ~glm2(formula5, data = .))
-  model4  <- map(dataconN_cv$train, ~glm2(formula4, data = .))
-  model3  <- map(dataconN_cv$train, ~glm2(formula3, data = .))
-  model2  <- map(dataconN_cv$train, ~glm2(formula2, data = .))
-  model1  <- map(dataconN_cv$train, ~glm2(formula1, data = .))
+  model7  <- purrr::map(dataconN_cv$train, ~glm2(formula7, data = .))
+  model6  <- purrr::map(dataconN_cv$train, ~glm2(formula6, data = .))
+  model5  <- purrr::map(dataconN_cv$train, ~glm2(formula5, data = .))
+  model4  <- purrr::map(dataconN_cv$train, ~glm2(formula4, data = .))
+  model3  <- purrr::map(dataconN_cv$train, ~glm2(formula3, data = .))
+  model2  <- purrr::map(dataconN_cv$train, ~glm2(formula2, data = .))
+  model1  <- purrr::map(dataconN_cv$train, ~glm2(formula1, data = .))
   
   get_pred  <- function(model, test_data){
     data  <- as.data.frame(test_data)
-    pred  <- add_predictions(data, model)
+    pred  <- modelr::add_predictions(data, model)
     return(pred)
   }
-  pred7  <- map2_df(model7, dataconN_cv$test, get_pred, .id = "Run")
-  pred6  <- map2_df(model6, dataconN_cv$test, get_pred, .id = "Run")
-  pred5  <- map2_df(model5, dataconN_cv$test, get_pred, .id = "Run")
-  pred4  <- map2_df(model4, dataconN_cv$test, get_pred, .id = "Run")
-  pred3  <- map2_df(model3, dataconN_cv$test, get_pred, .id = "Run")
-  pred2  <- map2_df(model2, dataconN_cv$test, get_pred, .id = "Run")
-  pred1  <- map2_df(model1, dataconN_cv$test, get_pred, .id = "Run")
+  pred7  <- purrr::map2_df(model7, dataconN_cv$test, get_pred, .id = "Run")
+  pred6  <- purrr::map2_df(model6, dataconN_cv$test, get_pred, .id = "Run")
+  pred5  <- purrr::map2_df(model5, dataconN_cv$test, get_pred, .id = "Run")
+  pred4  <- purrr::map2_df(model4, dataconN_cv$test, get_pred, .id = "Run")
+  pred3  <- purrr::map2_df(model3, dataconN_cv$test, get_pred, .id = "Run")
+  pred2  <- purrr::map2_df(model2, dataconN_cv$test, get_pred, .id = "Run")
+  pred1  <- purrr::map2_df(model1, dataconN_cv$test, get_pred, .id = "Run")
   
   MSE7  <- pred7 %>% group_by(Run) %>%
     dplyr::summarise(MSE = mean( (LCCadapt - pred)^2))
